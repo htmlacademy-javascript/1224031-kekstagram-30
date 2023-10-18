1560b905e20088afab9a08bb3313ee42658841ca
Keks <keks@htmlacademy.ru>
2023-10-18 21:00:43 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,29 @@
+const getStringLength = (string, maxLength) => string.length <= maxLength;
+//Вызов функции чтобы линтер не ругался
+getStringLength('проверяемая строка', 20);
+
+const palindromeCheck = (string) => {
+  string = string.toLowerCase().replaceAll(' ', '');
+  return Array.from(string).toString() === Array.from(string).reverse().toString();
+};
+//Вызов функции чтобы линтер не ругался
+palindromeCheck('топот');
+
+const getNumberFromString = (string) => {
+  let numbers = '';
+
+  string = string.toString();
+
+  for (let i = 0; i <= string.length - 1; i++) {
+    if (!isNaN(Number(string[i]))) {
+      numbers += string[i];
+    }
+  }
+
+  numbers = numbers.replaceAll(' ', '');
+
+  return parseInt(numbers, 10);
+};
+
+//Вызов функции чтобы линтер не ругался
+getNumberFromString('2023 год');
