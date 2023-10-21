40f7afeb7a2e194eec080c4b46699812373dff13
Torcher01 <miskichekov@gmail.com>
2023-10-21 15:40:40 +0700

    Добавляет функцию перевода времени в минуты и переводит время в минуты

--- js/functions.js
+++ js/functions.js
@@ -27,3 +27,18 @@ const getNumberFromString = (string) => {
 
 //Вызов функции чтобы линтер не ругался
 getNumberFromString('2023 год');
+
+//Решение задачи "Делу время"
+const timeInMinutes = (array) => (parseInt(array[0], 10) * 60) + (parseInt(array[1], 10));
+
+const isMeetingTimeCorrect = (workDayStart, workDayEnd, meetingStart, meetingTime) => {
+  const workDayStartInMinutes = timeInMinutes(workDayStart.split(':'));
+  const workDayEndInMinutes = timeInMinutes(workDayEnd.split(':'));
+  const meetingStartInMinutes = timeInMinutes(meetingStart.split(':'));
+};
+
+//console.log(isMeetingTimeCorrect('08:00', '17:30', '14:00', 90)) // true
+// console.log(isMeetingTimeCorrect('8:0', '10:0', '8:0', 120))     // true
+// console.log(isMeetingTimeCorrect('08:00', '14:30', '14:00', 90)) // false
+// console.log(isMeetingTimeCorrect('14:00', '17:30', '08:0', 90))  // false
+// console.log(isMeetingTimeCorrect('8:00', '17:30', '08:00', 900)) // false
