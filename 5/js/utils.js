4fcff7432570d3ed7c6282b3c98f928967d062c0
Keks <keks@htmlacademy.ru>
2023-10-25 07:32:36 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/utils.js
@@ -0,0 +1,39 @@
+//Генератор случайных чисел в заданном диапазоне
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+//Функция для извлечения случайной строки из массива
+const randomIndexFromArray = (array) => getRandomInteger(0, array.length - 1);
+
+//Функция-генератор ID
+function createIdGenerator () {
+  let lastGeneratedId = 0;
+
+  return function () {
+    lastGeneratedId += 1;
+    return lastGeneratedId;
+  };
+}
+
+//Функция-генератор случайного числа в заданном диапазоне
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+export {getRandomInteger, randomIndexFromArray, createIdGenerator, createRandomIdFromRangeGenerator};
