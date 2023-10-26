4c0efbea48a5998860f87b26e50df7f212810799
Torcher01 <miskichekov@gmail.com>
2023-10-26 21:53:08 +0700

    Создаёт функцию отрисовки изображений в новом модуле

---
+++ js/getPictures.js
@@ -0,0 +1,24 @@
+import {getPhotoArray} from './data.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const picturesArrayObj = getPhotoArray();
+const picturesFragment = document.createDocumentFragment();
+
+const getPictures = () => {
+  picturesArrayObj.forEach(({url, description, likes, comments}) => {
+    const picture = pictureTemplate.cloneNode(true);
+    picture.querySelector('.picture__img').src = url;
+    picture.querySelector('.picture__img').alt = description;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+    picturesFragment.append(picture);
+  });
+
+  return picturesContainer.append(picturesFragment);
+};
+
+
+export {getPictures};
