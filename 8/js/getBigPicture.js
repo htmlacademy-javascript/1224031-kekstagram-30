8100439f07170c7f07384b724eba294c0f9f3156
Keks <keks@htmlacademy.ru>
2023-11-06 20:58:11 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/getBigPicture.js
@@ -0,0 +1,85 @@
+import {getPictures, picturesContainer, picturesArrayObj} from './getPictures.js';
+import {isEscapeKey} from './utils.js';
+
+getPictures();
+
+const bigPicturePopup = document.querySelector('.big-picture');
+const bigPictureCloseButton = document.querySelector('.big-picture__cancel');
+
+const bigPictureImage = bigPicturePopup.querySelector('.big-picture__img').querySelector('img');
+const bigPictureLikesCount = bigPicturePopup.querySelector('.likes-count');
+const bigPictureCommentsCount = bigPicturePopup.querySelector('.social__comment-shown-count');
+const bigPictureCommentsTotalCount = bigPicturePopup.querySelector('.social__comment-total-count');
+const bigPictureDescription = bigPicturePopup.querySelector('.social__caption');
+
+const bigPictureComments = document.querySelector('.social__comments');
+const bigPictureCommentsTemplate = document.querySelector('#commentary')
+  .content
+  .querySelector('.social__comment');
+const commentsFragment = document.createDocumentFragment();
+
+//const bigPictureCommentsCounter = bigPicturePopup.querySelector('.social__comment-count');
+const bigPictureCommentsLoader = bigPicturePopup.querySelector('.comments-loader');
+
+const closeBigPicture = () => {
+  bigPicturePopup.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  bigPictureCloseButton.removeEventListener('click', closeBigPicture);
+};
+
+const openBigPicture = () => {
+  bigPicturePopup.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  bigPictureCloseButton.addEventListener('click', closeBigPicture);
+};
+
+const onEscCloseBigPicture = (evt) => {
+  if (isEscapeKey(evt)) {
+    bigPicturePopup.classList.add('hidden');
+    picturesContainer.removeEventListener('keydown', onEscCloseBigPicture);
+  }
+};
+
+const getBigPictureComments = (array) => {
+  bigPictureComments.innerHTML = '';
+  array.forEach((value) => {
+    const comment = bigPictureCommentsTemplate.cloneNode(true);
+    const avatar = comment.querySelector('.social__picture');
+    const commentText = comment.querySelector('.social__text');
+    avatar.src = value.avatar;
+    avatar.alt = value.name;
+    commentText.textContent = value.message;
+    commentsFragment.append(comment);
+  });
+  return bigPictureComments.append(commentsFragment);
+};
+const getMoreComments = (obj) => {
+  if (obj.comments.length <= 5) {
+    bigPictureCommentsLoader.classList.add('hidden');
+    bigPictureCommentsCount.textContent = obj.comments.length;
+  } else {
+    bigPictureCommentsLoader.classList.remove('hidden');
+    obj.comments = obj.comments.slice(0,5);
+    bigPictureCommentsCount.textContent = obj.comments.length;
+  }
+};
+
+const getBigPicture = (obj) => {
+  bigPictureImage.src = obj.url;
+  bigPictureLikesCount.textContent = String(obj.likes);
+  bigPictureCommentsTotalCount.textContent = String(obj.comments.length);
+  bigPictureDescription.textContent = obj.description;
+  getMoreComments(obj);
+};
+
+
+picturesContainer.addEventListener('click', (evt) => {
+  picturesArrayObj.forEach((value) => {
+    if (value.id === Number(evt.target.dataset.id)) {
+      openBigPicture();
+      getBigPicture(value);
+      getBigPictureComments(value.comments);
+      picturesContainer.addEventListener('keydown', onEscCloseBigPicture);
+    }
+  });
+});
---
+++ 8/js/getPictures.js
@@ -0,0 +1,25 @@
+import {getPhotoArray} from './getPhotoArray.js';
+
+const picturesContainer = document.querySelector('.pictures');
+const pictureTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+const picturesArrayObj = getPhotoArray();
+const picturesFragment = document.createDocumentFragment();
+
+const getPictures = () => {
+  picturesArrayObj.forEach(({id, url, description, likes, comments}) => {
+    const picture = pictureTemplate.cloneNode(true);
+    const photo = picture.querySelector('.picture__img');
+    photo.dataset.id = id;
+    photo.src = url;
+    photo.alt = description;
+    picture.querySelector('.picture__likes').textContent = likes;
+    picture.querySelector('.picture__comments').textContent = comments.length;
+    picturesFragment.append(picture);
+  });
+
+  return picturesContainer.append(picturesFragment);
+};
+
+export {getPictures, picturesContainer, picturesArrayObj};
