5197d7e6f187331b6bca7276278d5f604a0260e8
Torcher01 <miskichekov@gmail.com>
2023-11-08 12:26:08 +0700

    Переносит логику слушателей в функции

--- js/formValidate.js
+++ js/formValidate.js
@@ -9,32 +9,37 @@ const uploadCloseButton = document.querySelector('.img-upload__cancel');
 const photoCommentInputField = imgUploadForm.querySelector('.text__description');
 const photoHashtagsInputField = imgUploadForm.querySelector('.text__hashtags');
 
-imgUploadInput.addEventListener('change', () => {
-  imgUploadInterface.classList.remove('hidden');
-  document.querySelector('body').classList.add('modal-open');
-});
-
-uploadCloseButton.addEventListener('click', () => {
-  imgUploadInterface.classList.add('hidden');
-  document.querySelector('body').classList.remove('modal-open');
-  imgUploadInput.value = '';
-});
-document.addEventListener('keydown', (evt) => {
-  if (photoCommentInputField === document.activeElement || photoHashtagsInputField === document.activeElement) {
-    evt.stopPropagation();
-  }
-  if(isEscapeKey(evt)) {
-    imgUploadInterface.classList.add('hidden');
-    document.querySelector('body').classList.remove('modal-open');
-    imgUploadInput.value = '';
-  }
-});
-
 const pristine = new Pristine(imgUploadForm, {
   classTo: 'img-upload__field-wrapper',
   errorTextParent: 'img-upload__field-wrapper',
   errorTextClass: 'img-upload__field-wrapper--error',
 });
+
+const closeModal = () => {
+  imgUploadInterface.classList.add('hidden');
+  document.querySelector('body').classList.remove('modal-open');
+  imgUploadInput.value = '';
+  imgUploadForm.reset();
+  pristine.reset();
+  uploadCloseButton.removeEventListener('click', closeModal);
+};
+
+const openModal = () => {
+  imgUploadInterface.classList.remove('hidden');
+  document.querySelector('body').classList.add('modal-open');
+  uploadCloseButton.addEventListener('click', closeModal);
+};
+
+imgUploadInput.addEventListener('change', openModal);
+document.addEventListener('keydown', (evt) => {
+  if (photoCommentInputField === document.activeElement || photoHashtagsInputField === document.activeElement) {
+    evt.stopPropagation();
+  }
+  if(isEscapeKey(evt)) {
+    closeModal();
+  }
+});
+
 //Валидация описания фотографии
 const validateComment = (value) => value.length <= 140;
 
