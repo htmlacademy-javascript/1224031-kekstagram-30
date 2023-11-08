caf4057b5918b268fa5dc1ff69c5a265dfdd5f98
Torcher01 <miskichekov@gmail.com>
2023-11-08 12:17:44 +0700

    Заканчивает реализацию валидации полей с комментарием и хэш-тегами

--- js/formValidate.js
+++ js/formValidate.js
@@ -1,9 +1,13 @@
 import {isEscapeKey} from './utils.js';
 
+const hashtagRegular = /^#[a-zа-яё0-9]{1,19}$/i;
+
 const imgUploadInput = document.querySelector('.img-upload__input');
 const imgUploadForm = document.querySelector('.img-upload__form');
 const imgUploadInterface = document.querySelector('.img-upload__overlay');
 const uploadCloseButton = document.querySelector('.img-upload__cancel');
+const photoCommentInputField = imgUploadForm.querySelector('.text__description');
+const photoHashtagsInputField = imgUploadForm.querySelector('.text__hashtags');
 
 imgUploadInput.addEventListener('change', () => {
   imgUploadInterface.classList.remove('hidden');
@@ -15,7 +19,10 @@ uploadCloseButton.addEventListener('click', () => {
   document.querySelector('body').classList.remove('modal-open');
   imgUploadInput.value = '';
 });
-imgUploadForm.addEventListener('keydown', (evt) => {
+document.addEventListener('keydown', (evt) => {
+  if (photoCommentInputField === document.activeElement || photoHashtagsInputField === document.activeElement) {
+    evt.stopPropagation();
+  }
   if(isEscapeKey(evt)) {
     imgUploadInterface.classList.add('hidden');
     document.querySelector('body').classList.remove('modal-open');
@@ -25,16 +32,44 @@ imgUploadForm.addEventListener('keydown', (evt) => {
 
 const pristine = new Pristine(imgUploadForm, {
   classTo: 'img-upload__field-wrapper',
-  errorClass: 'img-upload__field-wrapper--error',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper--error',
 });
-const validateComment = () => true;
+//Валидация описания фотографии
+const validateComment = (value) => value.length <= 140;
 
 pristine.addValidator(
-  imgUploadForm.querySelector('.text__description'),
+  photoCommentInputField,
   validateComment,
   'Не более 140 символов'
 );
+//Валидация хэш-тегов
+const getHashtagNormalize = (tagString) => tagString
+  .toLowerCase()
+  .trim()
+  .split(' ')
+  .filter((tag) => Boolean(tag.length));
+const validateHashtagSymbols = (value) => getHashtagNormalize(value).every((tag) => hashtagRegular.test(tag));
+const validateHashtagsCount = (value) => getHashtagNormalize(value).length <= 5;
+const validateHashtagsUnic = (value) => getHashtagNormalize(value).length === new Set(getHashtagNormalize(value)).size;
+
+pristine.addValidator(
+  photoHashtagsInputField,
+  validateHashtagSymbols,
+  'Введите корректный хэш-тег'
+);
+pristine.addValidator(
+  photoHashtagsInputField,
+  validateHashtagsCount,
+  'Не более 5 хэш-тегов'
+);
+pristine.addValidator(
+  photoHashtagsInputField,
+  validateHashtagsUnic,
+  'Хэш-теги не должны повторяться'
+);
 imgUploadForm.addEventListener('submit', (evt) => {
-  evt.preventDefault();
-  pristine.validate();
+  if (!pristine.validate()) {
+    evt.preventDefault();
+  }
 });
